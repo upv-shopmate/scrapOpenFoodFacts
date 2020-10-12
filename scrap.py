9aac07781145b82d94c26089e3ddf9e01b6ac913
SaltyKawaiiNeko <software@skneko.moe>
2020-10-12 23:50:05 +0200

    [WIP] Adapt output to ShopMate DB schema

--- scrap.py
+++ scrap.py
@@ -3,10 +3,14 @@ from bs4 import BeautifulSoup
 import re
 import random
 
+from countries import countries_ES
+
 MAIN_URL = "https://world.openfoodfacts.org"
 URL = "https://es.openfoodfacts.org/tienda/mercadona"
-LIMIT = 162
+LIMIT = 20
+
 products = {}
+
 def get_html(url, n=None):
     page = None
     if n:
@@ -19,97 +23,77 @@ def get_html(url, n=None):
 def get_products():
     ul = soup.find("ul", "products")
     for a in ul.findChildren("a", recursive=True):
-        title = a.get("title", None)
+        title = a.get("title", None).strip()
         link = re.sub(r"producto", "product", MAIN_URL + a.get("href", None))
-        products[title] = {
+        gtin = re.match(r".*\/product\/(\d+)", link).group(1)
+        products[gtin] = {
+            "name": title,
             "link": link 
         }
-        get_product(title, link)
+        get_product(gtin, link)
 
         
-def get_product(title, link):
+def get_product(gtin, link):
     product_page_html = get_html(link)
     product_soup = BeautifulSoup(product_page_html, 'html.parser')
-    get_weight(title, product_soup)
-    get_origin(title, product_soup)
-    get_brand(title, product_soup)
-    get_categories(title, product_soup)
-    get_nutrients(title, product_soup)
-    get_image(title, product_soup)
-    get_barcode(title, product_soup)
-    get_price(title)
-    get_stock(title)
-    get_edible(title)
-    get_times_sold(title)
+    get_weight(gtin, product_soup)
+    get_origin(gtin, product_soup)
+    get_brand(gtin, product_soup)
+    get_categories(gtin, product_soup)
+    get_images(gtin, product_soup)
+    get_price(gtin)
+    get_stock(gtin)
+    get_edible(gtin)
+    get_times_sold(gtin)
 
 
-def get_edible(title):
-    products[title]["edible"] = True
+def get_edible(gtin):
+    products[gtin]["edible"] = True
 
-def get_barcode(title, product_soup):
-    barcode_span = product_soup.find("span", id="barcode")
-    if barcode_span is not None:
-        barcode = int(barcode_span.text)
-    else:
-        barcode = None
-    products[title]["image"] = barcode
-
-def get_image(title, product_soup):
+def get_images(gtin, product_soup):
     image_link = product_soup.find("img", id="og_image")
     if image_link is not None:
-        products[title]["image"] = image_link["src"]
+        products[gtin]["images"] = [image_link["src"]]
     else:
-        products[title]["image"] = None
+        products[gtin]["images"] = []
 
-def get_stock(title):
-    stock = random.randint(0, 50)
-    products[title]["stock"] = stock
+def get_stock(gtin):
+    stock = random.randint(0, 1000)
+    products[gtin]["available_stock"] = stock
 
-def get_times_sold(title):
-    times_sold = random.randint(0, 50)
-    products[title]["times_sold"] = times_sold
+def get_times_sold(gtin):
+    times_sold = random.randint(0, 1000)
+    products[gtin]["times_sold"] = times_sold
 
-def get_price(title):
+def get_price(gtin):
     price = round(random.uniform(0, 4), 2)
-    products[title]["price"] = price
+    products[gtin]["price"] = price
 
-def get_nutrients(title, product_soup):
-    nutrients = {}
-    tbody = product_soup.find("tbody")
-    for tr in tbody.findAll("tr"):
-        tds = tr.findAll("td")
-        nutrient_label = tds[0].text.strip()
-        if len(tds) > 1:
-            nutrient_value = tds[1].text.strip()
-            nutrients[nutrient_label] = nutrient_value
-        else:
-            nutrients[nutrient_label] = None
-    products[title]["nutrients"] = nutrients
-
-        
-def get_brand(title, product_soup):
+def get_brand(gtin, product_soup):
     brand_span = product_soup.find("span", text="Brands:")
     if brand_span is not None:
-        brand = re.sub(r"Brands:", "", brand_span.parent.text).strip()
-        products[title]["brand"] = brand
+        brands = re.sub(r"Brands:", "", brand_span.parent.text).strip().split(", ")
+        products[gtin]["brands"] = brands
 
-def get_categories(title, product_soup):
+def get_categories(gtin, product_soup):
     categories_span = product_soup.find("span", text="Categories:")
     if categories_span is not None:
         categories = re.sub(r"Categories:", "", categories_span.parent.text).strip().split(", ")
-        products[title]["categories"] = categories
+        products[gtin]["categories"] = categories
 
-def get_origin(title, product_soup):
+def get_origin(gtin, product_soup):
     origin_span = product_soup.find("span", text="Origin of ingredients:")
     if origin_span is not None:
-        origin = re.sub(r"Origin of ingredients:", "", origin_span.parent.text).strip()
-        products[title]["origin"] = origin
+        tokens = re.split(r",|\s", re.sub(r"Origin of ingredients:", "", origin_span.parent.text).strip())
+        origin = next(value for key, value in countries_ES.items() if any(token.lower() in key.lower() for token in tokens))
+        products[gtin]["origin_country"] = origin
 
-def get_weight(title, product_soup):
+def get_weight(gtin, product_soup):
     quantity_span = product_soup.find("span", text="Quantity:")
     if quantity_span is not None:
-        quantity = re.sub(r"Quantity:", "", quantity_span.parent.text).strip()
-        products[title]["weight"] = quantity
+        with_unit = re.sub(r"Quantity:", "", quantity_span.parent.text).strip()
+        quantity = int(''.join(filter(str.isdigit, with_unit)))
+        products[gtin]["weight"] = quantity
     
 
 def save():
